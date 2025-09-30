3efdc53722812caf66bf7e71eed58d70381a63d3
Chu A. <chu.amk22@gmail.com>
2025-09-30 16:53:29 +0100

    feat: implemented the 'get current weather by city' feature

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,35 @@
 'use strict'
+const apiKey = '26f067e98b7e4aa7b3f152800252609'
+const search = document.querySelector('.search-bar')
+const searchBtn = document.querySelector('.search-btn')
+const city = document.querySelector('.city-name')
+const condition = document.querySelector('.condition')
+const currentTemp = document.querySelector('.currentTemp')
+const state = document.querySelector('.state')
+const humidity = document.querySelector('.humidity-det')
+const windSpeed = document.querySelector('.wind-speed-det')
+
+// Fetch the current weather
+async function getCurrentWeather(location) {
+  const apiCurrentURL = `http://api.weatherapi.com/v1/current.json?key=26f067e98b7e4aa7b3f152800252609&q=${location}`
+  const response = await fetch(apiCurrentURL)
+  const data = await response.json()
+  console.table(data)
+  city.innerHTML = data.location.name
+  condition.innerHTML = data.current.condition.text
+  currentTemp.innerHTML = `Feels like ${data.current.temp_c} &deg;C`
+  state.src = data.current.condition.icon
+}
+
+async function getAirCondition(location) {
+  const apiCurrentURL = `http://api.weatherapi.com/v1/current.json?key=26f067e98b7e4aa7b3f152800252609&q=${location}`
+  const response = await fetch(apiCurrentURL)
+  const data = await response.json()
+  humidity.textContent = `${data.current.humidity}%`
+  windSpeed.innerHTML = `${data.current.wind_kph} Km/h`
+}
+
+searchBtn.addEventListener('click', () => {
+  getCurrentWeather(search.value)
+  getAirCondition(search.value)
+})
